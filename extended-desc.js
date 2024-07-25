28e797117e35656877a1a8a015d2eb185838fe28
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-07-25 10:20:05 +0200

    Update extended-desc.js

--- extended-desc.js
+++ extended-desc.js
@@ -1,8 +1,8 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
-    name: 'Extended description',
+    name: 'Extended description (Highpoint MOD)',
     version: '1.0',
-    author: 'NoobishSVK (Highpoint MOD)',
+    author: 'NoobishSVK',
     frontEndPath: 'extended-desc/frontend.js'
 }
 
